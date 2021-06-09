b29d04a4e264fe4dce56fd00f7073bf95c312718
fabiokleis <fabiohkrc@gmail.com>
2021-06-09 17:49:44 -0300

    created again core opts

--- src/server/server.js
+++ src/server/server.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const app = express();
 const usersRoute = require('./route/usersRoute');
+const cors = require('cors');
 
+const coreOpt = {
+    origin: 'https://fabiokleis.herokuapp.com'
+}
 
-app.use('/users',usersRoute);
+app.use(cors(coreOpt));
+
+app.use('/users', usersRoute);
 
 app.use((err, req, res, next) => {
 
