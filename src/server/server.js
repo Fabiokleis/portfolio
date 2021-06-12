4f47920254de150bd63ec2b88d05d28cc5ae4d20
fabiokleis <fabiohkrc@gmail.com>
2021-06-11 22:07:06 -0300

    again

--- src/server/server.js
+++ src/server/server.js
@@ -2,17 +2,17 @@ const express = require('express');
 const app = express();
 const usersRoute = require('./route/usersRoute');
 
-
+/*
 app.use((req, res, next) => {
 
-    res.header('Access-Control-Allow-Origin', 'fabiokleis.herokuapp.com');
+    res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
     res.header('Access-Control-Allow-Headers', 'Authorization, Content-Type, Origin, Accept');
 
     next();
 })
 
-
+*/
 app.use('/users', usersRoute);
 
 app.use((err, req, res, next) => {
