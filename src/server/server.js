cecf8b73d06d88f28d5a16001092673500d54edc
fabiokleis <fabiohkrc@gmail.com>
2021-06-12 17:38:40 -0300

    again

--- src/server/server.js
+++ src/server/server.js
@@ -6,6 +6,8 @@ const usersRoute = require('./route/usersRoute');
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', 'https://fabiokleis.herokuapp.com');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Origin');
+    res.header('Access-Control-Allow-Methods', 'OPTIONS, POST, GET, PUT, DELETE');
+    
     res.next();
 });
 
