dee394497cbbc3c75830e6c73e2b8566a6d39f6a
fabiokleis <fabiohkrc@gmail.com>
2021-06-11 21:46:45 -0300

    again

--- src/server/server.js
+++ src/server/server.js
@@ -7,8 +7,8 @@ app.use((req, res, next) => {
 
     res.header('Access-Control-Allow-Origin', 'https://fabiokleis.herokuapp.com');
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    res.header('Access-Control-Allow-Headers', 'Authorization, Content-Type');
-    res.header('Access-Control-Allow-Credentials', true);
+    res.header('Access-Control-Allow-Headers', 'Authorization, Content-Type, Origin, Accept');
+
     next();
 })
 
